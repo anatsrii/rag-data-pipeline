d34e717a0cc303cc969b55c14a12c440570f42d5
anatsrii <anat.allsolution@gmail.com>
2026-03-01 18:32:34 +0700

    Remove database, logs and test files from git

--- test_encode.py
+++
@@ -1,9 +0,0 @@
-# Test encoding fix
-text = b'\xc3\xa2\xc2\x80\xc2\x98Apps\xc3\xa2\xc2\x80\xc2\x99'.decode('utf-8')
-print('Input:', repr(text))
-
-# Apply replacements  
-text = text.replace('â\x80\x98', "'")
-text = text.replace('â\x80\x99', "'")
-
-print('Output:', repr(text))
